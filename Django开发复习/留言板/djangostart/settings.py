0acdf454402b63b6fcab52cdc914b610448610aa
Kevin0918 <2473998520@qq.com>
2017-06-04 08:38:20 +0800

    17.6.3

--- Django开发复习/留言板/djangostart/settings.py
+++ Django开发复习/留言板/djangostart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'message'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -123,11 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-STATIC_ROOT = '/static/'
 
 
 
--- Django开发复习/留言板/message/views.py
+++ Django开发复习/留言板/message/views.py
@@ -1,5 +1,28 @@
+# _*_ coding: utf_8 _*_
 from django.shortcuts import render
 
+from .models import UserMessage
+
 # Create your views here.
-def getform(requests):
-    return render(requests, 'messqge_form.html')
+def getform(request):
+
+    #all_message = UserMessage.objects.filter(name='bobby', address='北京')
+    #for message in all_message:
+    #   print message.name
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        message = request.POST.get('name', '')
+        adress = request.POST.get('adress', '')
+        email = request.POST.get('email', '')
+
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.address = adress
+        user_message.email = email
+        user_message.object_id = "helloworld3"
+
+        user_message.save()
+
+    return render(request, 'message_form.html')
+
